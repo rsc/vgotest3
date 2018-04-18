a4fc66f0f13a4f7ff97fa7e3c685ec8dc31c4af7
Russ Cox <rsc@golang.org>
2018-04-18 02:52:02 -0400

    use of new import paths

--- x.go
+++ x.go
@@ -1,4 +1,4 @@
 package vgotest3
 
-import _ "github.com/rsc/vgotest-backward"
-import _ "github.com/rsc/vgotest-backward/sub/dir"
+import _ "github.com/rsc/vgotest-backward/v2"
+import _ "github.com/rsc/vgotest-backward/v2/sub/dir"
